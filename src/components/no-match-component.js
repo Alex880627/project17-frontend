150d45b28ffded817d5ad2f224036d03e91d0232
Illés Alexander <illes.alexander88@gmail.com>
2019-08-08 16:42:40 +0200

    fixed some wievport issues

--- src/components/no-match-component.js
+++ src/components/no-match-component.js
@@ -4,6 +4,7 @@ const NoMatchComponent = () => (
   <div className="nomatch-comp">
     <form action="/">
       <h1>{"404"}</h1>
+      <h3>{"A keresett oldal nem található!"}</h3>
       <button type="submit">{"Vissza a főoldalra"}</button>
     </form>
   </div>
