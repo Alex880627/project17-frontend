db90de64e7a48a19f7f5d6e0dfb01e4e317ccc7d
Illés Alexander <illes.alexander88@gmail.com>
2019-11-13 20:25:12 +0100

    lazy tryout

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import LandinPageContainer from "./containers/landing-page-container";
 import GroupSessionsContainer from "./containers/group-sessions-container";
 import TherapistListContainer from "./containers/therapist-list-container";
-import PricesContainer from "./containers/prices-container";
+/* import PricesContainer from "./containers/prices-container"; */
 import TreatmentsListContainer from "./containers/treatments-list-container";
 import ContactsContainer from "./containers/contacts-container";
 import ScrollProgress from "./components/scroll-progress-bar"
 import FooterContainer from "./containers/footer-container";
 import SideDrawer from "./containers/side-drawer-cont";
+
+const PricesContainer = lazy(() => import("./containers/prices-container"));
 
 const App = () => {
   return (
@@ -17,7 +19,10 @@ const App = () => {
       <SideDrawer />
       <TreatmentsListContainer />
       <TherapistListContainer />
+      <Suspense fallback={"Loooooooooooooooading!"}>
       <PricesContainer />
+      </Suspense>  
+      
       <GroupSessionsContainer />
       <ContactsContainer />
       <FooterContainer />
